b5253839f117edc17ddc6b98c986d183ef90018e
Paul Brownsmith <paul.brownsmith@webstandardsdesign.co.uk>
2018-05-16 15:05:06 +0100

    Initial commit
    Setting up a NextJS app - https://nextjs.org/learn

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/MyLayout.js'
+import Header from '../components/Header'
+import Link from 'next/link'
+
+export default () => (
+    <Layout>
+       <p>Hello Next.js</p>
+    </Layout>
+)
